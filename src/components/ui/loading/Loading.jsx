863c4d967559845aa7e105e0d3c5d33d5350667f
Ahmad4433 <aliraza126005#gmail.com>
2024-03-14 01:30:59 +0500

    dsdS

--- src/components/ui/loading/Loading.jsx
+++ src/components/ui/loading/Loading.jsx
@@ -1,14 +1,12 @@
-import React from 'react'
-import style from './loading.module.css'
-import {CircularProgress} from '@mui/material'
+import React from "react";
+import style from "./loading.module.css";
+import { CircularProgress } from "@mui/material";
 const Loading = () => {
   return (
-    <div className={style.model} >
-      
-      <CircularProgress/>
-
+    <div className={style.model}>
+      <CircularProgress />
     </div>
-  )
-}
+  );
+};
 
-export default Loading
+export default Loading;

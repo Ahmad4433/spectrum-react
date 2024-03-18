41d6bec2407f17ffeabedff277e9105f4c6f5744
Ahmad4433 <aliraza126005#gmail.com>
2024-03-19 01:50:38 +0500

    Fdfd

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,81 +1,69 @@
-import React, { useState } from 'react'
-import {Outlet} from 'react-router-dom'
-import Header from './header/Header'
-import Footer from './footer/Footer'
-import style from './layout.module.css'
-import {useSelector,useDispatch} from 'react-redux'
-import Loading from '../ui/loading/Loading'
-import {DragDropContext} from 'react-beautiful-dnd'
-import {dragActions} from '../../store/slices/drag-slice'
-import { tenetActions } from '../../store/slices/tenets-sllice'
+import React, { useState } from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
+import style from "./layout.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import Loading from "../ui/loading/Loading";
+import { DragDropContext } from "react-beautiful-dnd";
+import { dragActions } from "../../store/slices/drag-slice";
+import { tenetActions } from "../../store/slices/tenets-sllice";
 
 const Layout = () => {
-const dispatch = useDispatch()
-const isLoading = useSelector(state=>state.ui.loading)
+  const dispatch = useDispatch();
+  const isLoading = useSelector((state) => state.ui.loading);
 
+  const reorderData = (list, startIndex, endIndex) => {
+    const result = Array.from(list);
 
+    const [removed] = result.splice(startIndex, 1);
 
+    result.splice(endIndex, 0, removed);
+    return result;
+  };
 
-const reorderData = (list, startIndex, endIndex) => {
-  const result = Array.from(list);
+  const dragEnd = (result) => {
+    const { source, destination, draggableId } = result;
 
-  const [removed] = result.splice(startIndex, 1);
+    if (!result.destination || source.droppableId === destination.droppableId) {
+      // dispatch(dragActions.setDestinationIndex(destination.index))
+      // dispatch(dragActions.setSourceIndex(source.index))
+      return;
+    }
 
-  result.splice(endIndex, 0, removed);
-  return result;
-};
+    if (
+      source.droppableId === "droppable" &&
+      destination.droppableId === "someid"
+    ) {
+      const formatedId = draggableId.split("-")[1];
+      dispatch(dragActions.setItemId(formatedId));
+    }
 
+    if (
+      source.droppableId === "tenets" &&
+      destination.droppableId === "tenetArray"
+    ) {
+      const formatedId = draggableId.split("-")[1];
+      dispatch(tenetActions.setId(formatedId));
+    }
+  };
 
-const dragEnd = (result)=>{
-
-  const {source,destination,draggableId} = result
-
-
-
-  if (!result.destination || source.droppableId === destination.droppableId) {
-    // dispatch(dragActions.setDestinationIndex(destination.index))
-    // dispatch(dragActions.setSourceIndex(source.index))
-    return;
-  }
- 
-
-
-    
- if(source.droppableId ==='droppable' && destination.droppableId === 'someid'){
-  const formatedId = draggableId.split('-')[1]
-  dispatch(dragActions.setItemId(formatedId))
- }
-
-
-if( source.droppableId ==='tenets' && destination.droppableId ==='tenetArray'){
-  const formatedId = draggableId.split('-')[1]
-  dispatch(tenetActions.setId(formatedId))
-}
-
-}
-
-
-const ondragStart = ()=>{
-  if(window.navigator.vibrate){
-    window.navigator.vibrate(100)
-  }
-
-}
-
+  const ondragStart = () => {
+    if (window.navigator.vibrate) {
+      window.navigator.vibrate(100);
+    }
+  };
 
   return (
-
-    <DragDropContext onDragStart={ondragStart} onDragEnd={dragEnd} >
-    <div className={style.main} >
-      
-      {isLoading &&  <Loading/>}
-      <Header/>
-    <Outlet/>
-    <Footer/>
-    </div>
+    <DragDropContext onDragStart={ondragStart} onDragEnd={dragEnd}>
+      <div className={style.main}>
+        {isLoading && <Loading />}
+        <Header />
+        <Outlet />
+        <Footer />
+      </div>
     </DragDropContext>
+  );
+};
 
-  )
-}
-
-export default Layout
+export default Layout;
